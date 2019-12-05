dbbc31166b71bf1352ce1b7e8ec22a1d65662592
Student <example@example.com>
2019-12-04 19:14:08 -0800

    variables with functions added

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
+
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
 $(document).ready(function() {
   $("form#calculator").submit(function() {
     event.preventDefault();
